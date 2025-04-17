7a6a81748e556d7e0db652c3a59aa6dd5ae4d7bb
kartiaga <kaiqueartiagaluz@gmail.com>
2025-04-17 15:20:09 -0300

    quadro de missões

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+function App() {
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <div className="card w-[80%] lg:w-[50%] h-[70%] shadow-md rounded-sm transform ease-out duration-300 items-center p-10 gap-5">
+        <h1 className="text-5xl font-work font-bold w-fit text-center">
+          Quest To Do
+        </h1>
+      </div>
+    </div>
+  )
 }
 
+export default App;
